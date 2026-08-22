42f73563389fe200bf2d2f6c4934e177ba5f133c
nishanth1596 <nishanth1596@gmail.com>
2026-08-22 21:17:49 +0530

    add all starter images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import { Public_Sans } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Personal Finance App",
+  description: "A personal finance management application.",
+}
 
 const publicSans = Public_Sans({
   subsets: ["latin"],
